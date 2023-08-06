aea55dedf28b549a5b8ba3d03836098222ce5965
Ubuntu <ubuntu@ip-172-31-33-158.ap-south-1.compute.internal>
2023-08-06 12:32:45 +0000

    code file

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    port = 5000  # Change this to the desired port number
+    app.run(host='0.0.0.0', port=port)
+
